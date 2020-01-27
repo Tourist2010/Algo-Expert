fd7debc79879d50d613b1ca0b963ab74fcadbb75
Ajay <apsrivas123@gmail.com>
2020-01-27 08:01:04 +0530

    Solution to problem "Four Number Sum"

--- src/Arrays/FourNumberSum.java
+++ src/Arrays/FourNumberSum.java
@@ -1,45 +1,51 @@
 package Arrays;
 
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
+import java.util.*;
 
-/*Difficulty - EASY*/
+/*Difficulty - Medium*/
 public class FourNumberSum {
     public static void main(String[] args){
-        printList(moveElementToEnd(new ArrayList<>(Arrays.asList(3, 3, 3, 3, 3, 3, 3)), 3));
+        printList(Objects.requireNonNull(fourNumberSum(new int[]{1, 2, 3, 4, 5, 6, 7}, 10)));
     }
 
-    /*TimeComplexity - O(n) where n is the size of the list
-      Space Complexity - O(1) */
-    public static List<Integer> moveElementToEnd(List<Integer> array, int toMove) {
-        int right = array.size() - 1;
-        int left = 0;
+    /*TimeComplexity - O(n^2) where n is the size of the list | Worst case O(n^3)
+      Space Complexity - O(n^2) */
+    public static List<Integer[]> fourNumberSum(int[] array, int targetSum) {
+        List<Integer[]> res = new ArrayList<>();
+        Map<Integer, List<Integer[]>> sums = new HashMap<>();
 
-        while (right >= 0 && array.get(right) == toMove){
-            right--;
-        }
-
-        while (right >= 0 && left < right) {
-            if(array.get(left) == toMove){
-                array.set(left, array.get(right));
-                array.set(right, toMove);
-                while (right >= 0 && array.get(right) == toMove){
-                    right--;
+        for(int i = 1; i < array.length; i++){
+            for (int j = i+1; j < array.length; j++){
+                int add = array[i] + array[j];
+                int diff = targetSum - add;
+                if(sums.containsKey(diff)){
+                    for(int k = 0; k < sums.get(diff).size(); k++){
+                        Integer[] subArray = sums.get(diff).get(k);
+                        res.add(new Integer[]{subArray[0], subArray[1], array[i], array[j]});
+                    }
                 }
             }
-            left++;
+
+            for(int k = 0; k < i; k++){
+                int add = array[i] + array[k];
+                if(sums.containsKey(add)){
+                    sums.get(add).add(new Integer[]{array[i], array[k]});
+                } else
+                    sums.put(add, new ArrayList<Integer[]>(Collections.singleton(new Integer[]{array[i], array[k]})));
+            }
         }
-        return array;
+
+
+        return res;
     }
 
     /**
-     * method to print the list
+     * method to print the 2D list
      * @param list list to be printed
      */
-    public static void printList(List<Integer> list){
-        for(Integer i : list){
-            System.out.print(i + " ");
+    public static void printList(List<Integer[]> list){
+        for(Integer[] i : list){
+            System.out.println(i[0] + " " + i[1] + " " + i[2] + " " + i[3]);
         }
     }
 }
