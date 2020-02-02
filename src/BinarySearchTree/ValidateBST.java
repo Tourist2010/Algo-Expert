6efe94186f6d198c81d98403066a5b33634956fd
Ajay <apsrivas123@gmail.com>
2020-02-02 13:56:27 +0530

    Solution to problem "Validate Binary Search Tree"

--- src/BinarySearchTree/ValidateBST.java
+++ src/BinarySearchTree/ValidateBST.java
@@ -8,16 +8,14 @@ public class ValidateBST {
     //O(n) time | O(d) space, where d is the depth of the tree
     public static boolean validBst(BST tree, int min, int max) {
         if(tree == null) return false;
-        if(tree.left == null && tree.right == null){
+        if(tree.left == null && tree.right == null)
             return (tree.value >= min && tree.value < max);
-        }
         if(tree.left != null && tree.right == null)
             return ((tree.value > tree.left.value) && (tree.value >= min && tree.value < max)
                     && validBst(tree.left, min, tree.value));
         if(tree.left == null && tree.right != null)
             return ((tree.value <= tree.right.value) && (tree.value >= min && tree.value < max) &&
                     validBst(tree.right, tree.value, max));
-
         return (tree.value > tree.left.value && tree.value <= tree.right.value) && (tree.value >= min && tree.value < max) &&
                 validBst(tree.left, min, tree.value) && validBst(tree.right, tree.value, max);
     }
