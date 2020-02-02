caa91d1705976776848be2f63d1157681269c7a4
Ajay <apsrivas123@gmail.com>
2020-02-02 13:37:27 +0530

    Solution to problem "Binary Search Tree"

--- src/BinarySearchTree/BSTTraversal.java
+++ src/BinarySearchTree/BSTTraversal.java
@@ -1,4 +1,4 @@
-package Arrays;
+package BinarySearchTree;
 
 
 import java.util.List;
---
+++ src/BinarySearchTree/ValidateBST.java
@@ -0,0 +1,35 @@
+package BinarySearchTree;
+
+public class ValidateBST {
+    public static boolean validateBst(BST tree) {
+        return validBst(tree, Integer.MIN_VALUE, Integer.MAX_VALUE);
+    }
+
+    //O(n) time | O(d) space, where d is the depth of the tree
+    public static boolean validBst(BST tree, int min, int max) {
+        if(tree == null) return false;
+        if(tree.left == null && tree.right == null){
+            return (tree.value >= min && tree.value < max);
+        }
+        if(tree.left != null && tree.right == null)
+            return ((tree.value > tree.left.value) && (tree.value >= min && tree.value < max)
+                    && validBst(tree.left, min, tree.value));
+        if(tree.left == null && tree.right != null)
+            return ((tree.value <= tree.right.value) && (tree.value >= min && tree.value < max) &&
+                    validBst(tree.right, tree.value, max));
+
+        return (tree.value > tree.left.value && tree.value <= tree.right.value) && (tree.value >= min && tree.value < max) &&
+                validBst(tree.left, min, tree.value) && validBst(tree.right, tree.value, max);
+    }
+
+    //Binary Search Tree class
+    static class BST {
+        public int value;
+        public BST left;
+        public BST right;
+
+        public BST(int value) {
+            this.value = value;
+        }
+    }
+}
