bbff4a78382bfa6992a0ae5884fe06291f7dae60
Ajay <apsrivas123@gmail.com>
2020-02-02 13:57:23 +0530

    Solution to problem "Binary Search Tree Traversals"

--- src/BinarySearchTree/BSTTraversal.java
+++ src/BinarySearchTree/BSTTraversal.java
@@ -9,9 +9,7 @@ public class BSTTraversal {
     public static List<Integer> inOrderTraverse(BST tree, List<Integer> array) {
         if(tree.left != null)
             inOrderTraverse(tree.left, array);
-
         array.add(tree.value);
-
         if(tree.right != null)
             inOrderTraverse(tree.right, array);
         return array;
@@ -20,13 +18,10 @@ public class BSTTraversal {
     //O(n) time | O(n) space
     public static List<Integer> preOrderTraverse(BST tree, List<Integer> array) {
         array.add(tree.value);
-
         if(tree.left != null)
             preOrderTraverse(tree.left, array);
-
         if(tree.right != null)
             preOrderTraverse(tree.right, array);
-
         return array;
     }
 
@@ -34,12 +29,9 @@ public class BSTTraversal {
     public static List<Integer> postOrderTraverse(BST tree, List<Integer> array) {
         if(tree.left != null)
             postOrderTraverse(tree.left, array);
-
         if(tree.right != null)
             postOrderTraverse(tree.right, array);
-
         array.add(tree.value);
-
         return array;
     }
 
